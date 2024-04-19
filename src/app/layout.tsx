aa56817bfb53f6ef1369bc31962a7b7d3f05233f
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-04-19 12:31:14 +0900

    vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/header';
@@ -26,6 +27,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
