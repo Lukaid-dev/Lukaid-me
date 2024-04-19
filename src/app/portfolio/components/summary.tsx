5489b08880f0e2f24aec655b30022a35e5fd8956
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-04-19 15:41:50 +0900

    [refactor] summary 조절

--- src/app/portfolio/components/summary.tsx
+++ src/app/portfolio/components/summary.tsx
@@ -5,12 +5,12 @@ export default function Summary({ summary }: { summary: summary }) {
   return (
     <div className="text-text">
       {summary.isUrl ? (
-        <div className="flex items-center gap-2 overflow-auto text-nowrap">
+        <div className="flex items-center gap-2 text-nowrap">
           <span className="shrink-0 font-semibold">{summary.key}</span>
           <div className="relative size-4 shrink-0">
             <Image fill src="/vertical-divider.svg" alt="vertical-divider" />
           </div>
-          <div className="flex shrink-0 gap-2">
+          <div className="flex gap-2 overflow-auto">
             {summary.urls?.map((url, index) => (
               <div key={url.title} className="flex shrink-0 gap-2">
                 <a
@@ -26,12 +26,12 @@ export default function Summary({ summary }: { summary: summary }) {
           </div>
         </div>
       ) : (
-        <div className="flex items-center gap-2 overflow-auto text-nowrap">
+        <div className="flex items-center gap-2 text-nowrap">
           <span className="shrink-0 font-semibold">{summary.key}</span>
           <div className="relative size-4 shrink-0">
             <Image fill src="/vertical-divider.svg" alt="vertical-divider" />
           </div>
-          <span className="flex gap-2">{summary.value}</span>
+          <span className="flex overflow-auto">{summary.value}</span>
         </div>
       )}
     </div>
