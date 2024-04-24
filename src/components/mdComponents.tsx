277c66c611b244d335f8b2d46ecb1b7031063ff0
Seongwoo_Lukaid <crescent70004536@gmail.com>
2024-04-25 01:12:11 +0900

    마크다운 지원 문법 정리

--- src/components/mdComponents.tsx
+++ src/components/mdComponents.tsx
@@ -20,6 +20,15 @@ type Components = Partial<{
 }>;
 
 const markdownComponents: Components = {
+  h1: ({ node, ...props }) => (
+    <h1 className="my-4 text-4xl text-text lg:text-4xl" {...props} />
+  ),
+  h2: ({ node, ...props }) => (
+    <h2 className="my-4 text-3xl text-text lg:text-2xl" {...props} />
+  ),
+  h3: ({ node, ...props }) => (
+    <h3 className="my-4 text-2xl text-text lg:text-xl" {...props} />
+  ),
   img: ({ node, ...props }) => (
     // eslint-disable-next-line jsx-a11y/alt-text
     <Image
@@ -37,15 +46,15 @@ const markdownComponents: Components = {
     const Icon = getIcon(language!);
     if (!language) return <pre className="m-2" {...props} />;
     return (
-      <div className="m-2 flex flex-col rounded-lg bg-code-bg">
+      <div className="bg-back_layout m-2 flex flex-col rounded-lg">
         <div className=" flex items-center justify-between px-4 py-2">
-          <div className="flex items-center gap-4 text-gray-400">
+          <div className="flex items-center gap-4 text-text">
             <span className="text-lg">{Icon}</span>
             <span className="text-xs">{language}</span>
           </div>
           <button
             data-clipboard=""
-            className="text-lg text-gray-400 active:text-green-500">
+            className="active:text-green-500 text-lg text-text">
             <BiClipboard />
           </button>
         </div>
@@ -59,22 +68,54 @@ const markdownComponents: Components = {
     );
     if (!currentLanguage) return <code className="text-white" {...props} />;
     return (
-      <SyntaxHighlighter
-        language={currentLanguage}
-        showLineNumbers={true}
-        style={oneDark}
-        lineNumberStyle={{
-          color: 'rgba(255, 255, 255, 0.5)',
-          fontSize: '0.8rem',
-          marginRight: '1rem',
-        }}>
-        {String(props.children).replace(/\n$/, '')}
-      </SyntaxHighlighter>
+      <div className="bg-back_layout p-2">
+        <SyntaxHighlighter
+          language={currentLanguage}
+          showLineNumbers={true}
+          style={oneDark}
+          lineNumberStyle={{
+            color: 'rgba(255, 255, 255, 0.5)',
+            fontSize: '0.8rem',
+            marginRight: '1rem',
+          }}>
+          {String(props.children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      </div>
     );
   },
   a: ({ node, ...props }) => {
-    return <a target="_blank" {...props} />;
+    return <a className="text-text" target="_blank" {...props} />;
   },
+  table: ({ node, ...props }) => (
+    <table className="my-4 table-auto border-collapse" {...props} />
+  ),
+  thead: ({ node, ...props }) => (
+    <thead className="rounded-t-lg bg-guide text-text" {...props} />
+  ),
+  tbody: ({ node, ...props }) => (
+    <tbody className="bg-back_layout rounded-b-lg" {...props} />
+  ),
+  th: (props) => (
+    <th
+      scope="col"
+      className="px-6 py-1.5 font-semibold text-gray-900 first:rounded-tl-lg last:rounded-tr-lg"
+      {...props}>
+      {props.children}
+    </th>
+  ),
+  tr: (props) => (
+    <tr className="border-b last:border-none" {...props}>
+      {props.children}
+    </tr>
+  ),
+  td: (props) => (
+    <td className="px-6 py-3" {...props}>
+      {props.children}
+    </td>
+  ),
+  blockquote: ({ node, ...props }) => (
+    <blockquote className="text-text" {...props} />
+  ),
 };
 
 export default markdownComponents;
